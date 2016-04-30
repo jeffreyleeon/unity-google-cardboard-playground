4ac00de7bd5cb72a42a2738c9a649fbb6083eed4
JeffreyLee <jeffreyleeon@gmail.com>
2016-04-30 10:31:47 +0800

    First commit with vr camera sdk embedded

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void FixedUpdate() {
+		Vector3 vec = new Vector3(0f, 0f, 1f * Time.deltaTime);
+		transform.Translate(vec);
+	}
+}
